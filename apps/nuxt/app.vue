459be11801215b8840be212f17dc5407a9369a8a
Rupert Dunk <rupert@rupertdunk.com>
2023-09-04 17:57:41 +0100

    fix: move shamefully-hoist to root npmrc, align nuxt layout

--- apps/nuxt/app.vue
+++ apps/nuxt/app.vue
@@ -1,7 +1,5 @@
 <template>
-  <main
-    class="flex min-h-screen flex-col items-center justify-center p-24 gap-24"
-  >
+  <main class="flex min-h-screen flex-col items-center justify-between p-24">
     <div
       class="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"
     >
